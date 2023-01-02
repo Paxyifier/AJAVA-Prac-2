0c19171fc8e1775ae9e2548cc6c6beab51dc0291
Paxyifier <59118083+Paxyifier@users.noreply.github.com>
2023-01-02 11:12:56 +0000

    emergency

---
+++ prac3/src/main/java/student/prathamesh/App.java
@@ -0,0 +1,13 @@
+package student.prathamesh;
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class App 
+{
+    public static void main( String[] args )
+    {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("");
+		Student s1 = (Student) ctx.getBean("studentBean");
+		System.out.print(s1.toString());
+    }
+}
